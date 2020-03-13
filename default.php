03c246da34d9740f6a09fb2d05067cc3474ff98a
alistherbarreto <alisther1627@gmail.com>
2020-03-13 14:03:46 +0530

    Create default.php

---
+++ default.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>Feel Goa - Tours and Travels</title>
+
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+</head>
+<body>
+    <h1><center>Development Page</center></h1>
+</body>
+</html>
